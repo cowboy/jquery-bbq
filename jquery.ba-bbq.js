08e07c5b7b09a28b1f96b0525dbe0e2d9234efd5
Adriano Dadario <dev+dadario@vagas.com.br>
2013-02-05 17:55:01 -0200

    Keep interface of browser.msie, after jquery 1.9 removed from core. Same solution used by simple-modal

--- jquery.ba-bbq.js
+++ jquery.ba-bbq.js
@@ -1284,7 +1284,13 @@
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     // vvvvvvvvvvvvvvvvvvv REMOVE IF NOT SUPPORTING IE6/7/8 vvvvvvvvvvvvvvvvvvv
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    $.browser.msie && !supports_onhashchange && (function(){
+    var ua = navigator.userAgent.toLowerCase();
+    var browser = {
+      ieQuirks: null,
+      msie: /msie/.test(ua) && !/opera/.test(ua),
+      opera: /opera/.test(ua)
+    };
+    browser.msie && !supports_onhashchange && (function(){
       // Not only do IE6/7 need the "magical" Iframe treatment, but so does IE8
       // when running in "IE7 compatibility" mode.
       
