f8c0a8c47db28cf34225376b07764174936c434c
James Addison <jay@jp-hosting.net>
2020-05-06 15:48:52 +0100

    Standalone keys: indenting and whitespace cleanup

--- jquery.ba-bbq.js
+++ jquery.ba-bbq.js
@@ -528,42 +528,42 @@
             : val;                                                // string
         }
       }
-        
-        if ( keys_last ) {
-          // Complex key, build deep object structure based on a few rules:
-          // * The 'cur' pointer starts at the object top-level.
-          // * [] = array push (n is set to array length), [n] = array if n is 
-          //   numeric, otherwise object.
-          // * If at the last keys part, set the value.
-          // * For each keys part, if the current level is undefined create an
-          //   object or array based on the type of the next keys part.
-          // * Move the 'cur' pointer to the next level.
-          // * Rinse & repeat.
-          for ( ; i <= keys_last; i++ ) {
-            key = keys[i] === '' ? cur.length : keys[i];
-            cur = cur[key] = i < keys_last
-              ? cur[key] || ( keys[i+1] && isNaN( keys[i+1] ) ? {} : [] )
-              : val;
-          }
-          
-        } else if ( key ) {
-          // Simple key, even simpler rules, since only scalars and shallow
-          // arrays are allowed.
-          
-          if ( $.isArray( obj[key] ) ) {
-            // val is already an array, so push on the next value.
-            obj[key].push( val );
-            
-          } else if ( obj[key] !== undefined ) {
-            // val isn't an array, but since a second value has been specified,
-            // convert val into an array.
-            obj[key] = [ obj[key], val ];
-            
-          } else {
-            // val is a scalar.
-            obj[key] = val;
-          }
+
+      if ( keys_last ) {
+        // Complex key, build deep object structure based on a few rules:
+        // * The 'cur' pointer starts at the object top-level.
+        // * [] = array push (n is set to array length), [n] = array if n is
+        //   numeric, otherwise object.
+        // * If at the last keys part, set the value.
+        // * For each keys part, if the current level is undefined create an
+        //   object or array based on the type of the next keys part.
+        // * Move the 'cur' pointer to the next level.
+        // * Rinse & repeat.
+        for ( ; i <= keys_last; i++ ) {
+          key = keys[i] === '' ? cur.length : keys[i];
+          cur = cur[key] = i < keys_last
+            ? cur[key] || ( keys[i+1] && isNaN( keys[i+1] ) ? {} : [] )
+            : val;
         }
+
+      } else if ( key ) {
+        // Simple key, even simpler rules, since only scalars and shallow
+        // arrays are allowed.
+
+        if ( $.isArray( obj[key] ) ) {
+          // val is already an array, so push on the next value.
+          obj[key].push( val );
+
+        } else if ( obj[key] !== undefined ) {
+          // val isn't an array, but since a second value has been specified,
+          // convert val into an array.
+          obj[key] = [ obj[key], val ];
+
+        } else {
+          // val is a scalar.
+          obj[key] = val;
+        }
+      }
     });
     
     return obj;
